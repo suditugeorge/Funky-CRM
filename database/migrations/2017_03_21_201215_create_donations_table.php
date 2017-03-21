caad1e17e9aeea2b704c5399fa2874de88b49803
Florin Frătică <florin.fratica@webdigital.ro>
2017-03-21 22:27:14 +0200

    InnoDB engine added

--- database/migrations/2017_03_21_201215_create_donations_table.php
+++ database/migrations/2017_03_21_201215_create_donations_table.php
@@ -14,6 +14,7 @@ class CreateDonationsTable extends Migration
     public function up()
     {
         Schema::create('donations', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
             $table->increments('id');
             $table->integer('donor_id')->unsigned();
             $table->foreign('donor_id')->references('id')->on('donors');
