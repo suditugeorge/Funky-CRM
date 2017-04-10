05006e87d1e5cd9d173159c5c623deec5ce046c3
Florin Frătică <florin.fratica@webdigital.ro>
2017-04-10 20:05:55 +0300

    Search Volunteers API endpoint

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,8 @@
 <?php
 
-use Illuminate\Http\Request;
+Route::group(['middleware' => 'throttle:10000'], function () {
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+    // Volunteers
+    Route::get('/volunteers', 'Api\volunteersController@index');
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
 });
